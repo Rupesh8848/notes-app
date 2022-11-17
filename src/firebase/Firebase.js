417cd90d918f81368830c42194b931296e22266d
Rupesh <rupesh@pop-os.localdomain>
2022-11-17 09:06:32 +0545

    First Commit

---
+++ src/firebase/Firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAIA8Q68ZgNIZrvX66ssdUzxDdFWF88roo",
+  authDomain: "notes-app-2e3fd.firebaseapp.com",
+  projectId: "notes-app-2e3fd",
+  storageBucket: "notes-app-2e3fd.appspot.com",
+  messagingSenderId: "965915396687",
+  appId: "1:965915396687:web:edbf359bf30b4396b1b83a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
+
