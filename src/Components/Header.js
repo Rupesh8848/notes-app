b9aee8ca9a4b2ac34bfe9831c17c8c21259e9762
Rupesh8848 <shrestharupesh02@gmail.com>
2022-11-17 16:10:27 +0545

    Header added with navigate functionalities

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import {SlDiamond} from "react-icons/sl"
+import { Navigate, useNavigate } from 'react-router-dom'
 export default function Header() {
+    const navigate = useNavigate()
   return (
     <>
-        
+        <div className='p-4 flex justify-between sticky top-0 bg-white'>
+            <div className='flex justify-center items-center'>
+                <SlDiamond/>
+                <p>PicStore</p>
+            </div>
+            <div className=''>
+                <ul className='flex justify-center items-center'>
+                    <li className='mr-10 cursor-pointer' onClick={()=>navigate("/")}>Home</li>
+                    <li className='mr-6 cursor-pointer' onClick={()=>navigate("/sign-in")}>SignIn</li>
+                    <li></li>
+                </ul>
+            </div>
+         </div>
+
+         
+
     </>
   )
 }
